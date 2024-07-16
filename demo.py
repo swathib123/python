ab7bc200b99f1673405818c12008175458f0eb44
swathib123 <swathibarre89@gmail.com>
2024-07-16 12:20:18 +0530

    the third commit

--- demo.py
+++ demo.py
@@ -1,2 +1,9 @@
-for number in range(1,10):
+for num in range(1,10):
+    pass
+for number in range(1,16):
+    if number == 15:
+        break
+    else:
+        if number==5:
+            continue
     print(number)
